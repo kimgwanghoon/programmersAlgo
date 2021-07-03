fba8e79a16e4cd968f0d7d0da6af138a0477ac09
KimGwangHoon <khkimykk@naver.com>
2021-07-04 01:36:57 +0900

    행렬의곱셈

---
+++ test/src/test/MatrixMultipl.java
@@ -0,0 +1,27 @@
+package test;
+
+public class MatrixMultipl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr1= {{1, 4}, {3, 2}, {4, 1}},arr2= {{3,3},{3,3}};
+		solution(arr1,arr2);
+	}
+	
+	public static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        int sum=0;
+        
+        for(int i=0; i<arr1.length;i++){
+            for(int j=0;j<arr2[0].length;j++){
+                for(int k=0;k<arr2.length;k++){
+                    sum=sum+(arr1[i][k]*arr2[k][j]);
+                }
+                answer[i][j]=sum;
+                sum=0;
+            }
+        }
+        
+        return answer;
+    }
+}
